ed7b5a3eac1b1d9a63101c105334abeeeacb9530
thiagoluizfb <thiagoluizfb@gmail.com>
2020-08-14 23:24:00 +0000

    Space numbers fixed; Bluetoken coordinates set; Loop around board spaces function tested

--- assets/scripts/tokens.js
+++ assets/scripts/tokens.js
@@ -1,1 +1,20 @@
-let mleft = [0];
+$("#blueTokenOne").click(function(){
+
+let blueleft = [55,80,105,130,130,130,130,130,130,155,180,180,180,180,180,180,205,230,255,280,305,305,305,280,255,230,205,180,180,180,180,180,180,155,130,130,130,130,130,130,105,80,55,30,5,5,30,55,80,105,130];
+let bluetop = [180,180,180,180,205,230,255,280,305,305,305,280,255,230,205,180,180,180,180,180,180,155,130,130,130,130,130,130,105,80,55,30,5,5,5,30,55,80,105,130,130,130,130,130,130,155,155,155,155,155,155];
+
+var myVar = setInterval(myTimer, 500);
+
+let i = 52 //Math.floor(Math.random() * 10+1);
+let j = 0;
+
+function myTimer() {
+  $("#blueTokenOne").css({"margin-left": blueleft[j]+"px","position": "absolute"});
+  $("#blueTokenOne").css({"margin-top": bluetop[j]+"px","position": "absolute"});
+  $("#blueTokenOne").html(j+1);
+  j++;
+  if (j==i){
+  var clear = clearInterval(myVar);
+}
+}
+});
