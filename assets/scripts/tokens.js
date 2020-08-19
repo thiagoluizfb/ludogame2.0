663b1be65093c46c0cf757bb9f976f0881d67416
thiagoluizfb <thiagoluizfb@gmail.com>
2020-08-19 01:52:51 +0000

    Separete first round from rollthedice function, Defined new element to start the first round, removed the 5th element from result array, testing if second largest is equal to the largest one

--- assets/scripts/tokens.js
+++ assets/scripts/tokens.js
@@ -13,32 +13,49 @@ numberofplayers = 0;
 players = ["blue","yellow","red","green"];
 
 
-function offdice(i){
-    $("#"+players[i]+"dice").off("click");
-    if(i<4){
-        rollthedice(i+1);
-    }
-    if (i==3){
-        setTimeout(function(){alert (players[results.indexOf(Math.max(...results))] + "  starts!")},1000);
-    }
-}
 
-function rollthedice(i){
-    //$("#"+players[i]+"dice").click(function (){
+function whostarts(i){
         var dieone = Math.floor(Math.random()*6+1);
         var dietwo = Math.floor(Math.random()*6+1);
         $("#"+players[i]+"diceone").html(dieone);
         $("#"+players[i]+"dicetwo").html(dietwo);
         window.results[i] = dieone + dietwo;
         setTimeout(function(){
-            offdice(i);
+            $("#"+players[i]+"dice").off("click");
+            if(i<4){
+                whostarts(i+1);
+            }
+            if (i==3){
+                setTimeout(function(){
+                    alert(results);
+                    results.pop();
+                    winner = results;
+                    winner.sort();
+                if (winner[0]==winner[1]){
+                    alert ("two winners " + winner +"results:"+ results);
+                    $("#start").css("z-index","1");
+                    
+                }else{
+                $("#"+players[results.indexOf(Math.max(...results))]+"dice").on("click",function(){rollthedice(results.indexOf(Math.max(...results)))});
+                setTimeout(function(){alert (players[results.indexOf(Math.max(...results))] + "  starts! " + results)},1000);
+                }},1000);
+            };
         },1000);
-//});
 }
 
-$("#blueTokenOne").click(function(){
+function rollthedice(i){
+        var dieone = Math.floor(Math.random()*6+1);
+        var dietwo = Math.floor(Math.random()*6+1);
+        $("#"+players[i]+"diceone").html(dieone);
+        $("#"+players[i]+"dicetwo").html(dietwo);
+        window.results[i] = dieone + dietwo;
+        $("#"+players[i]+"dice").off("click");
+};
+
+$("#start").click(function starts(){
+    $(this).css("z-index","-1");
     let i = 0;
-    rollthedice(i);
+    whostarts(i);
 });
 
 
@@ -50,13 +67,13 @@ let blueleft = [55,80,105,130,130,130,130,130,130,155,180,180,180,180,180,180,20
 let bluetop = [180,180,180,180,205,230,255,280,305,305,305,280,255,230,205,180,180,180,180,180,180,155,130,130,130,130,130,130,105,80,55,30,5,5,5,30,55,80,105,130,130,130,130,130,130,155,155,155,155,155,155];
 
 var myVar = setInterval(myTimer, 500);
-let i  = window.resultblue;
+let i  = window.results[0];
 let j = 0;
 newpos = blueonepos+i;
 
 function myTimer() {
     /*$("#blueTokenTwo").css("z-index","-1");*/
-$("#blueTokenOne").css("z-index","1");
+    $("#blueTokenOne").css("z-index","1");
   $("#blueTokenOne").css({"margin-left": blueleft[newpos-i+j]+"px","position": "absolute"});
   $("#blueTokenOne").css({"margin-top": bluetop[newpos-i+j]+"px","position": "absolute"});
   $("#blueTokenOne").html(newpos);
