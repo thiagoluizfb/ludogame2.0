ce2fb194557551e2b4a624c9c9fc3f0be480811b
thiagoluizfb <thiagoluizfb@gmail.com>
2020-08-25 14:25:58 +0000

    Reorganizing and testing functions: check if there are tokens inside home => if there is any and any dice is 5, move tokens to first position

--- assets/scripts/tokens.js
+++ assets/scripts/tokens.js
@@ -8,19 +8,33 @@ $("#numPlayersFour").click(function(){
     localStorage.setItem("numplayer", 4);
 });
 
-results = [0,0,0,0];
-token = ["One","Two","Three","Four"];
-numberofplayers = 0;
-players = ["blue","yellow","red","green"];
+let numberofplayers = 0;
+let players = ["blue","yellow","red","green"];
+let token = ["One","Two","Three","Four"];
+
+let redleft = [255,	230,205,180,180,180,180,180,180,155,130,130,130,130,130,130,105,80,55,30,5,5,5,30,55,80,105,130,130,130,130,130,130,155,180,180,180,180,180,180,205,230,255,280,305,305,280,255,230,205,180];
+let redtop = [130,130,130,130,105,80,55,30,5,5,5,30,55,80,105,130,130,130,130,130,130,155,180,180,180,180,180,180,205,230,255,280,305,305,305,280,255,230,205,180,180,180,180,180,180,130,130,130,130,130,130];
+let blueleft = [55,80,105,130,130,130,130,130,130,155,180,180,180,180,180,180,205,230,255,280,305,305,305,280,255,230,205,180,180,180,180,180,180,155,130,130,130,130,130,130,105,80,55,30,5,5,30,55,80,105,130];
+let bluetop = [180,180,180,180,205,230,255,280,305,305,305,280,255,230,205,180,180,180,180,180,180,155,130,130,130,130,130,130,105,80,55,30,5,5,5,30,55,80,105,130,130,130,130,130,130,155,155,155,155,155,155];
+let greenleft = [130,130,130,130,105,80,55,30,5,5,5,30,55,80,105,130,130,130,130,130,130,155,180,180,180,180,180,180,205,230,255,280,305,305,305,280,255,230,205,180,180,180,180,180,180,155,155,155,155,155,155];
+let greentop = [55,80,105,130,130,130,130,130,130,155,180,180,180,180,180,180,205,230,255,280,305,305,305,280,255,230,205,180,180,180,180,180,180,155,130,130,130,130,130,130,105,80,55,30,5,5,30,55,80,105,130];        
+let yellowleft = [180,180,180,180,205,230,255,280,305,305,305,280,255,230,205,180,180,180,180,180,180,155,130,130,130,130,130,130,105,80,55,30,5,5,5,30,55,80,105,130,130,130,130,130,130,155,155,155,155,155,155];
+let yellowtop = [255,230,205,180,180,180,180,180,180,155,130,130,130,130,130,130,105,80,55,30,5,5,5,30,55,80,105,130,130,130,130,130,130,155,180,180,180,180,180,180,205,230,255,280,305,305,280,255,230,205,180];
+
+var tomove = [];
 var dieone = [0,0,0,0];
 var dietwo = [0,0,0,0];
+var results = [0,0,0,0];
 var position = [[0,0,0,0],[0,0,0,0],[0,0,0,0],[0,0,0,0]];
+var out = [[0,0,0,0],[0,0,0,0],[0,0,0,0],[0,0,0,0]];
+
+$(".board").hide();
 
 $("#play").click(function starts(){
+    $(".board").show();
     $("#start").css("z-index","-1");
     $("#layer").css("background-image","none");
     let i = 0;
-    let m = i;
     whostarts(i);
 });
 
@@ -56,16 +70,9 @@ function whostarts(i){
 }
 
 function game(i){
-    //i=3;
-
-    let redleft = [255,	230,205,180,180,180,180,180,180,155,130,130,130,130,130,130,105,80,55,30,5,5,5,30,55,80,105,130,130,130,130,130,130,155,180,180,180,180,180,180,205,230,255,280,305,305,280,255,230,205,180];
-    let redtop = [130,130,130,130,105,80,55,30,5,5,5,30,55,80,105,130,130,130,130,130,130,155,180,180,180,180,180,180,205,230,255,280,305,305,305,280,255,230,205,180,180,180,180,180,180,130,130,130,130,130,130];
-    let blueleft = [55,80,105,130,130,130,130,130,130,155,180,180,180,180,180,180,205,230,255,280,305,305,305,280,255,230,205,180,180,180,180,180,180,155,130,130,130,130,130,130,105,80,55,30,5,5,30,55,80,105,130];
-    let bluetop = [180,180,180,180,205,230,255,280,305,305,305,280,255,230,205,180,180,180,180,180,180,155,130,130,130,130,130,130,105,80,55,30,5,5,5,30,55,80,105,130,130,130,130,130,130,155,155,155,155,155,155];
-    let greenleft = [130,130,130,130,105,80,55,30,5,5,5,30,55,80,105,130,130,130,130,130,130,155,180,180,180,180,180,180,205,230,255,280,305,305,305,280,255,230,205,180,180,180,180,180,180,155,155,155,155,155,155];
-    let greentop = [55,80,105,130,130,130,130,130,130,155,180,180,180,180,180,180,205,230,255,280,305,305,305,280,255,230,205,180,180,180,180,180,180,155,130,130,130,130,130,130,105,80,55,30,5,5,30,55,80,105,130];        
-    let yellowleft = [180,180,180,180,205,230,255,280,305,305,305,280,255,230,205,180,180,180,180,180,180,155,130,130,130,130,130,130,105,80,55,30,5,5,5,30,55,80,105,130,130,130,130,130,130,155,155,155,155,155,155];
-    let yellowtop = [255,230,205,180,180,180,180,180,180,155,130,130,130,130,130,130,105,80,55,30,5,5,5,30,55,80,105,130,130,130,130,130,130,155,180,180,180,180,180,180,205,230,255,280,305,305,280,255,230,205,180];
+    i=3;
+    $("#dicemoveone").show();
+    $("#dicemovetwo").show();
    
     if(i===0){
         x = blueleft;
@@ -89,8 +96,7 @@ function game(i){
 
     $("#layer").off("click");
     $("#"+players[i]+"dice").css("z-index","3");
-    $("#"+players[i]+"dice").one("click",function() {
-        rollthedice(i);});
+    $("#"+players[i]+"dice").one("click",function() {rollthedice(i);});
     $("#"+players[i]+"TokenOne, #"+players[i]+"TokenTwo, #"+players[i]+"TokenThree, #"+players[i]+"TokenFour").on("click",function(){
         $("#"+players[i]+"TokenOne, #"+players[i]+"TokenTwo, #"+players[i]+"TokenThree, #"+players[i]+"TokenFour").css("z-index","1");
         tokenposition = $(this).position();
@@ -99,54 +105,84 @@ function game(i){
         tokenchose(i);
     });
     $("#dicemoveone").one("click", function(){move(i)});
+    $("#dicemovetwo").one("click", function(){move(i)});
 }
 
 function rollthedice(i){
-       // alert("rollthedice"+players[i]);
-        $("#"+players[i]+"dice").css("z-index","1");
-        dieone[i] = Math.floor(Math.random()*6+1);
-        dietwo[i] = Math.floor(Math.random()*6+1);
-        $("#"+players[i]+"diceone").html(dieone[i]);
-        $("#"+players[i]+"dicetwo").html(dietwo[i]);
-        if (dieone[i] == 5){
-            dieone[i] = 0;
-            as(i);
-            choosetoken(i);
-        }else{
-            if (dietwo[i] == 5){
-            dietwo[i] = 0;
-            leavehome(i);
-            choosetoken(i);
-            }else{            
-            choosetoken(i);
-            }
-        }
-        return;
+    $("#"+players[i]+"dice").css("z-index","1");
+    dieone[i] = Math.floor(Math.random()*6+1);
+    dietwo[i] = Math.floor(Math.random()*6+1);
+    $("#"+players[i]+"diceone").html(dieone[i]);
+    $("#"+players[i]+"dicetwo").html(dietwo[i]);
+    checkfive(i);
+    return;
 }
 
-function leavehome(i) {  
-    $("#"+players[i]+"Token"+token[position[i].indexOf(0)]).css({"left": x[0]+"px","position": "absolute"});
-    $("#"+players[i]+"Token"+token[position[i].indexOf(0)]).css({"top": y[0]+"px","position": "absolute"});
-    $("#dicemovetwo").hide();
-    initpos = 0;
-    position[i][position[i].indexOf(0)]=initpos;
+function checkfive(i){
+    var hasfive = dieone[i] == 5 || dietwo[i] == 5;
+    if (hasfive==true){
+        alert("Has five");
+        if(out[i].includes(0)){
+            if (dieone[i] == 5){
+                alert("Die one is 5");
+                dieone[i] = 0;
+                $("#dicemoveone").hide();
+                leavehome(i);
+            };
+            if(dietwo[i]==5){
+                alert("Die two is 5");
+                if(out[i].includes(0)){
+                    if (dieone[i] == 5){
+                        dieone[i] = 0;
+                        $("#dicemoveone").hide();
+                        leavehome(i);
+                    };
+                };
+            return;
+            };
+        };
+    };
+}
+
+function leavehome(i) {
+    $("#"+players[i]+"Token"+token[out[i].indexOf(0)]).css({"left": x[0]+"px","position": "absolute"});
+    $("#"+players[i]+"Token"+token[out[i].indexOf(0)]).css({"top": y[0]+"px","position": "absolute"});
+    /*$("#"+players[i]+"Token"+token[position[i].indexOf(0)]).children().css("z-index","1");
+    $("#"+players[i]+"Token"+token[position[i].indexOf(0)]).children().css("height","24px");
+    $("#"+players[i]+"Token"+token[position[i].indexOf(0)]).children().css("width","24px");
+    $("#"+players[i]+"Token"+token[position[i].indexOf(0)]).children().css("margin-left","-6px");
+    $("#"+players[i]+"Token"+token[position[i].indexOf(0)]).children().css("margin-top","-6px");
+    //$("#"+players[i]+"Token"+token[position[i].indexOf(0)]).children().children().html(tomove);*/
+    $("#"+players[i]+"Token"+token[out[i].indexOf(0)]).css("z-index","3");
+    alert("#"+players[i]+"Token"+token[out[i].indexOf(0)]);
+    out[i][out[i].indexOf(0)] = 1;
+    alert(out);
     return;
     };
 
+
 function choosetoken(i){
-   // alert("choosetoken"+players[i]);
     $("#layer").off("click");
     $("#dicewrapper").css("z-index","-1");
     $("#dicewrapper").css("top","200px");
     $("#dicewrapper").css("left","200px");
     $("#"+players[i]+"dice").css("z-index","1");
-    $(".tokenwrapper"+players[i]).css("z-index","1");
-    $(".tokenwrapper"+players[i]).css("height","24px");
-    $(".tokenwrapper"+players[i]).css("width","24px");
-    $(".tokenwrapper"+players[i]).css("margin-left","-6px");
-    $(".tokenwrapper"+players[i]).css("margin-top","-6px");
-    $(".token"+players[i]).html(dieone[i]+","+dietwo[i]);
-    $("#"+players[i]+"TokenOne, #"+players[i]+"TokenTwo, #"+players[i]+"TokenThree, #"+players[i]+"TokenFour").css("z-index","3");
+
+
+    let n =0
+    for(n=0;n<4;n++){
+        if(out[i][n]>0){
+            //alert("#"+players[i]+"Token"+token[n]);
+           $("#"+players[i]+"Token"+token[n]).children().css("z-index","1");
+           $("#"+players[i]+"Token"+token[n]).children().css("height","24px");
+           $("#"+players[i]+"Token"+token[n]).children().css("width","24px");
+           $("#"+players[i]+"Token"+token[n]).children().css("margin-left","-6px");
+           $("#"+players[i]+"Token"+token[n]).children().css("margin-top","-6px");;
+           $("#"+players[i]+"Token"+token[n]).children().children().html(tomove);
+           $("#"+players[i]+"Token"+token[n]).css("z-index","3");
+           alert("#"+players[i]+"Token"+token[n]);
+        };
+    }
     return;
 }
 
@@ -192,12 +228,10 @@ function move(i){
         $("#"+players[i]+"Token"+token[movetoken]).css({"top": y[newpos-k+l]+"px","position": "absolute"});
         l++;
         if (l==k){
-            var clear = clearInterval(myVar);
+            clearInterval(myVar);
             position[i][j] += l;
            // alert(position[i]);
             if(i==3){
-                $("#dicemovetwo").show();
-                //$("#dicewrapper").css("width", "54px");
                 let i = 0;
                 game(i);
             }else{
