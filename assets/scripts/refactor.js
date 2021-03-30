98294eb17cb6ce90865d99ae55eaea7c7e8689ff
thiagoluizfb <thiagoluizfb@gmail.com>
2021-03-30 20:41:05 +0000

    Fix bugs 1: Define tokens properties and board positions as oriented objects

---
+++ assets/scripts/refactor.js
@@ -0,0 +1,26 @@
+var tokens = [
+    {token:0, color:1, playerType:1, homePosition:{x:25,y:226}, initialSlot:1, colorSlot:0, boardSlot:0, finalLane:0, dieOne:0, dieTwo:0, move:0},
+    {token:1, color:1, playerType:1, homePosition:{x:86.8,y:226}, initialSlot:1, colorSlot:0, boardSlot:0, finalLane:0, dieOne:0, dieTwo:0, move:0},
+    {token:2, color:1, playerType:1, homePosition:{x:25,y:290}, initialSlot:1, colorSlot:0, boardSlot:0, finalLane:0, dieOne:0, dieTwo:0, move:0},
+    {token:3, color:1, playerType:1, homePosition:{x:86.8,y:290}, initialSlot:1, colorSlot:0, boardSlot:0, finalLane:0, dieOne:0, dieTwo:0, move:0},
+    {token:0, color:2, playerType:1, homePosition:{x:225,y:226}, initialSlot:13, colorSlot:0, boardSlot:0, finalLane:0, dieOne:0, dieTwo:0, move:0},
+    {token:1, color:2, playerType:1, homePosition:{x:286.8,y:226}, initialSlot:13, colorSlot:0, boardSlot:0, finalLane:0, dieOne:0, dieTwo:0, move:0},
+    {token:2, color:2, playerType:1, homePosition:{x:225,y:290}, initialSlot:13, colorSlot:0, boardSlot:0, finalLane:0, dieOne:0, dieTwo:0, move:0},
+    {token:3, color:2, playerType:1, homePosition:{x:286.8,y:290}, initialSlot:13, colorSlot:0, boardSlot:0, finalLane:0, dieOne:0, dieTwo:0, move:0},
+    {token:0, color:3, playerType:1, homePosition:{x:225,y:26}, initialSlot:25, colorSlot:0, boardSlot:0, finalLane:0, dieOne:0, dieTwo:0, move:0},
+    {token:1, color:3, playerType:1, homePosition:{x:86.8,y:26}, initialSlot:25, colorSlot:0, boardSlot:0, finalLane:0, dieOne:0, dieTwo:0, move:0},
+    {token:2, color:3, playerType:1, homePosition:{x:225,y:90}, initialSlot:25, colorSlot:0, boardSlot:0, finalLane:0, dieOne:0, dieTwo:0, move:0},
+    {token:3, color:3, playerType:1, homePosition:{x:86.8,y:90}, initialSlot:25, colorSlot:0, boardSlot:0, finalLane:0, dieOne:0, dieTwo:0, move:0},
+    {token:0, color:4, playerType:1, homePosition:{x:25,y:26}, initialSlot:37, colorSlot:0, boardSlot:0, finalLane:0, dieOne:0, dieTwo:0, move:0},
+    {token:1, color:4, playerType:1, homePosition:{x:86.8,y:26}, initialSlot:37, colorSlot:0, boardSlot:0, finalLane:0, dieOne:0, dieTwo:0, move:0},
+    {token:2, color:4, playerType:1, homePosition:{x:25,y:90}, initialSlot:37, colorSlot:0, boardSlot:0, finalLane:0, dieOne:0, dieTwo:0, move:0},
+    {token:3, color:4, playerType:1, homePosition:{x:86.8,y:90}, initialSlot:37, colorSlot:0, boardSlot:0, finalLane:0, dieOne:0, dieTwo:0, move:0},
+]
+
+const board = [ {x:55,y:180},{x:80,y:180},{x:105,y:180},{x:130,y:180},{x:130,y:205},{x:130,y:230},{x:130,y:255},{x:130,y:280},
+                {x:130,y:305},{x:155,y:305},{x:180,y:305},{x:180,y:280},{x:180,y:255},{x:180,y:230},{x:180,y:205},{x:180,y:180},
+                {x:205,y:180},{x:230,y:180},{x:255,y:180},{x:280,y:180},{x:305,y:180},{x:305,y:155},{x:305,y:130},{x:280,y:130},
+                {x:255,y:130},{x:230,y:130},{x:205,y:130},{x:180,y:130},{x:180,y:105},{x:180,y:80},{x:180,y:55},{x:180,y:30},
+                {x:180,y:5},{x:155,y:5},{x:130,y:5},{x:130,y:30},{x:130,y:55},{x:130,y:80},{x:130,y:105},{x:130,y:130},
+                {x:105,y:130},{x:80,y:130},{x:55,y:130},{x:30,y:130},{x:5,y:130},{x:5,y:155},{x:5,y:180},{x:30,y:180}
+            ];
