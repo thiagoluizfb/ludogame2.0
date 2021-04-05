a1bec7877f9a318fd9c9d8f170c997cae62b6218
thiagoluizfb <thiagoluizfb@gmail.com>
2021-04-05 18:59:55 +0000

    Fix bugs 5: Add fixed sendhome, blockspace, unblockspace, whoishere and leavehome functions to refactor.js file

--- assets/scripts/refactor.js
+++ assets/scripts/refactor.js
@@ -1,9 +1,9 @@
 var tokens = [
-    {token:0, color:0, playerType:1, homePosition:{x:25,y:226}, initialSlot:1, colorSlot:48, boardSlot:48, finished:0, dieOne:0, dieTwo:0, move:20},
+    {token:0, color:0, playerType:1, homePosition:{x:25,y:226}, initialSlot:1, colorSlot:32, boardSlot:32, finished:0, dieOne:0, dieTwo:0, move:10},
     {token:1, color:0, playerType:1, homePosition:{x:86.8,y:226}, initialSlot:1, colorSlot:0, boardSlot:0, finished:0, dieOne:0, dieTwo:0, move:0},
     {token:2, color:0, playerType:1, homePosition:{x:25,y:290}, initialSlot:1, colorSlot:0, boardSlot:0, finished:0, dieOne:0, dieTwo:0, move:0},
     {token:3, color:0, playerType:1, homePosition:{x:86.8,y:290}, initialSlot:1, colorSlot:0, boardSlot:0, finished:0, dieOne:0, dieTwo:0, move:0},
-    {token:0, color:1, playerType:1, homePosition:{x:225,y:226}, initialSlot:13, colorSlot:0, boardSlot:0, finished:0, dieOne:0, dieTwo:0, move:0},
+    {token:0, color:1, playerType:1, homePosition:{x:225,y:226}, initialSlot:13, colorSlot:0, boardSlot:42, finished:0, dieOne:0, dieTwo:0, move:0},
     {token:1, color:1, playerType:1, homePosition:{x:286.8,y:226}, initialSlot:13, colorSlot:0, boardSlot:0, finished:0, dieOne:0, dieTwo:0, move:0},
     {token:2, color:1, playerType:1, homePosition:{x:225,y:290}, initialSlot:13, colorSlot:0, boardSlot:0, finished:0, dieOne:0, dieTwo:0, move:0},
     {token:3, color:1, playerType:1, homePosition:{x:286.8,y:290}, initialSlot:13, colorSlot:0, boardSlot:0, finished:0, dieOne:0, dieTwo:0, move:0},
@@ -17,7 +17,7 @@ var tokens = [
     {token:3, color:3, playerType:1, homePosition:{x:86.8,y:90}, initialSlot:37, colorSlot:0, boardSlot:0, finished:0, dieOne:0, dieTwo:0, move:0},
 ]
 
-let board = [ {x:55,y:180},{x:80,y:180},{x:105,y:180},{x:130,y:180},{x:130,y:205},{x:130,y:230},{x:130,y:255},{x:130,y:280},{x:130,y:305},{x:155,y:305},
+let board = [ {x:0,y:0}, {x:55,y:180},{x:80,y:180},{x:105,y:180},{x:130,y:180},{x:130,y:205},{x:130,y:230},{x:130,y:255},{x:130,y:280},{x:130,y:305},{x:155,y:305},
                 {x:180,y:305},{x:180,y:280},{x:180,y:255},{x:180,y:230},{x:180,y:205},{x:180,y:180},{x:205,y:180},{x:230,y:180},{x:255,y:180},{x:280,y:180},
                 {x:305,y:180},{x:305,y:155},{x:305,y:130},{x:280,y:130},{x:255,y:130},{x:230,y:130},{x:205,y:130},{x:180,y:130},{x:180,y:105},{x:180,y:80},
                 {x:180,y:55},{x:180,y:30},{x:180,y:5},{x:155,y:5},{x:130,y:5},{x:130,y:30},{x:130,y:55},{x:130,y:80},{x:130,y:105},{x:130,y:130},{x:105,y:130},
@@ -33,29 +33,36 @@ let finalLanes = [
 
 let lastSlot = 51;
 let rsafespace = [1,7,10,13,19,22,25,31,34,37,43,46];
-var rblockedposition = []
+var rblockedposition = [6,7,8]
 
 var dieone;
 var dietwo;
 var turn_color = 0;
 var player = tokens.filter(player => player.color == turn_color);
 
+rollthedice();
+checkFive();
+
+
 function rollthedice() { 
     dieone =  5;//Number(Math.floor(Math.random()*6+1));
     dietwo =  1;//Number(Math.floor(Math.random()*6+1));
     console.log(dieone);
-    console.log(dietwo);
+    console.log(dietwo); 
+    player.map(token => (token.dieOne = dieone, token.dieTwo = dietwo));
     return;
 }
 
-function checkFive() { 
+function checkFive() {
     if(dieone+dietwo == 5 || dieone == 5 || dietwo == 5){
         var initialSlot = player[0].initialSlot;
         var noBlock = tokens.filter(token => token.boardSlot == initialSlot).length < 2;
         var otherBlock = tokens.filter(token => token.boardSlot == initialSlot && token.color != turn_color);
         var myBlock = player.filter(mytoken => mytoken.boardSlot == initialSlot) == 2;
         if (noBlock){
+            thisToken =  player.find(thisToken => thisToken.boardSlot == 0);
             console.log(`Leave home`);
+            leavehome(thisToken);
         }else{
             if(myBlock){
                 console.log(`It's blocked by myself, showing other options`);
@@ -67,7 +74,6 @@ function checkFive() {
         console.log("No fives, going to options");
     }
 }
-
 
 function options() {
     for (token of player){
@@ -87,37 +93,108 @@ function options() {
 }
 
 function move(){
-    var token =  player.find(token => token.move > 0);
-    while (token.move >= 0 ){
-        if (token.colorSlot == lastSlot){
-            token.finished = 1
+    var thisToken =  player.find(thisToken => thisToken.move > 0);
+    var blockedSpace = rsafespace.includes(thisToken.boardSlot);
+    if(blockedSpace){
+        var otherToken = tokens.find(otherToken => otherToken.boardSlot == thisToken.boardSlot && otherToken.color != thisToken.color);
+        unblockspace(thisToken, otherToken, thisToken.boardSlot);
+    }
+    while (thisToken.move >= 0 ){
+        if(thisToken.move == 0){
+            whoishere(thisToken.color, thisToken.token);
+        }
+        if (thisToken.colorSlot == lastSlot){
+            thisToken.finished = 1
             console.log(`This token has finished`);
             return;
         }
-        if(token.colorSlot > 46){
-            var position = token.colorSlot-47;
-            console.log(`Player ${token.color}, token ${token.token}: Actual position is ${token.boardSlot} and there is ${token.move} moves left`);
-            console.log(`Coordinates are ${finalLanes[token.color][position].x} and ${finalLanes[token.color][position].y}`)
+        if(thisToken.colorSlot > 46){
+            var position = thisToken.colorSlot-47;
+            console.log(`Player ${thisToken.color}, token ${thisToken.token}: Actual position is ${thisToken.boardSlot} and there is ${thisToken.move} moves left`);
+            console.log(`Coordinates are ${finalLanes[thisToken.color][position].x} and ${finalLanes[thisToken.color][position].y}`)
         }else{
-            var position = token.boardSlot;
-            console.log(`Player ${token.color}, token ${token.token}: Actual position is ${position} and there is ${token.move} moves left`);
+            var position = thisToken.boardSlot;
+            console.log(`Player ${thisToken.color}, token ${thisToken.token}: Actual position is ${thisToken.boardSlot} and there is ${thisToken.move} moves left`);
             console.log(`Coordinates are ${board[position].x} and ${board[position].y}`)
         }
-        token.boardSlot += 1;
-        token.colorSlot += 1;
-        token.move -= 1;
+        thisToken.boardSlot += 1;
+        thisToken.colorSlot += 1;
+        thisToken.move -= 1;
     }
+    return;
 }
 
-function sendhome(color, token){
-    thistoken = tokens.find(thistoken => thistoken.color == color && thistoken.token == token);
-    thistoken.colorSlot = 0;
-    thistoken.boardSlot = 0;
-    xPosition = thistoken.homePosition.x;
-    yPosition = thistoken.homePosition.y;
-    console.log(thistoken, xPosition, yPosition);
+function whoishere(color, token){
+    var thisToken = tokens.find(thistoken => thistoken.color == color && thistoken.token == token);
+    var boardSlot = thisToken.boardSlot;
+    var otherToken = tokens.find(otherToken => otherToken.boardSlot == boardSlot && otherToken.color != color);
+    var safeSlot = rsafespace.includes(boardSlot);
+    // console.log(boardSlot);
+    // console.log(safespace);
+    // console.log(safeSlot);
+    if (otherToken){
+        if(safeSlot) {
+            console.log(`Blocking space, safeplace`);
+            blockspace(thisToken, otherToken, boardSlot);
+        }else{
+            if(otherToken.color == color){
+                blockspace(thisToken, otherToken, boardSlot);
+            }else{
+                if(boardSlot > 46){
+                    return;
+                }else{
+                    console.log(`Sending player ${otherToken.color} token ${otherToken.token} home`)
+                    sendhome(otherToken);
+                }
+            }  
+        }
+    }
+    return;
 }
 
-// rollthedice();
-// options();
-// sendhome(0,0);
+function sendhome(thisToken){
+    console.log(`sendding home`)
+    thisToken.colorSlot = 0;
+    thisToken.boardSlot = 0;
+    var xPosition = thisToken.homePosition.x;
+    var yPosition = thisToken.homePosition.y;
+    console.log(thisToken, xPosition, yPosition);
+    return;
+}
+
+function blockspace(thisToken, otherToken, boardSlot){
+    console.log(thisToken);
+    console.log(otherToken);
+    rblockedposition.push(boardSlot);
+    console.log(rblockedposition);
+    return;
+}
+
+function unblockspace(thisToken, otherToken, boardSlot){
+    console.log(rblockedposition);
+    console.log(thisToken);
+    console.log(otherToken);
+    rblockedposition.push(boardSlot);
+    console.log(rblockedposition);
+    rblockedposition.splice(rblockedposition.indexOf(boardSlot),1)
+    console.log(rblockedposition);
+    return;
+}
+
+function leavehome(thisToken) {
+    thisToken.boardSlot = thisToken.initialSlot
+    thisToken.colorSlot = 1
+    xPosition = board[thisToken.initialSlot].x
+    yPosition = board[thisToken.initialSlot].y
+    if (dieone == 5){
+        player.map(token => token.dieOne = 0)
+    }
+    if (dietwo == 5){
+        player.map(token => token.dieTwo = 0)
+    }
+    if (dieone + dietwo == 5){
+        player.map(token => (token.dieOne = 0, token.dieTwo = 0))
+    }
+    return;
+}
+
